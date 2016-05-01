444b3787e0bd9b15ff50da48f3a24856eef227c8
Bertram Varga <Bertram Varga>
2016-05-01 16:22:10 +0200

    solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a subclass for matrix that contains the inverse, S4 style - Yepp, in R, you can apparently subclass built-in types :/ - 
 
-## Write a short comment describing this function
+CachedMatrix = setClass("CachedMatrix", prototype = matrix(), slots = c(.Data='matrix', inverse='matrix'))
 
+## Just build the default stuff 
 makeCacheMatrix <- function(x = matrix()) {
+  CachedMatrix(x)
+}
 
+## The definitions for each version.
+cacheSolve <- function(x, ...){
+  UseMethod("cacheSolve", x)
+}
+
+cacheSolve.default <- function(x, ...){
+  stop("Argument is Not a matrix")
+}
+
+cacheSolve.matrix <- function(x, ...){
+  x = makeCacheMatrix(x)
+  cacheSolve(x)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve.CachedMatrix <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  if(length(x@inverse)==0){
+    x@inverse = solve(x)
+  }
+  x@inverse
 }
